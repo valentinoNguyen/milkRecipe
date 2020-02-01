b8e3e33af031e40f121a673aa6acca033c863db9
Tri Nguyen <tnguyen@atomiton.com>
2020-02-01 12:01:10 +0700

    - add lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
+import { BottomBarComponent } from './core/layouts/bottom-bar/bottom-bar.component';
+import { TopBarComponent } from './core/layouts/top-bar/top-bar.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        BottomBarComponent,
+        TopBarComponent,
+        MenuComponent,
+        RecipeComponent,
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         HttpClientModule,
+        MatButtonModule,
         ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
     ],
     providers: [],
